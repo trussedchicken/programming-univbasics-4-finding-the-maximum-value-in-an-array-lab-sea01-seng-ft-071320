f668b65244ef58be9ea378e009a87407b2369211
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-19 01:27:13 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,9 @@ def find_max_value(array)
   count = 0 
   while count < array.length do
     array = array.max_by do |element|
-  element.field
-end
+      element.field 
+    end
+  
+
     count += 1 
 end
